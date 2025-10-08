04f1bea6e2a7b439e223eb357a96b21f11a3048c
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-10-08 16:46:26 +0200

    Process only today and next 3 days

--- fetch-train-formation.sh
+++ fetch-train-formation.sh
@@ -4,9 +4,22 @@ PACKAGE_ID=occupancy-forecast-json-dataset
 PACKAGE_PATH=$DIR/openTdataCH--showcases/data/opentransportdata.swiss/$PACKAGE_ID
 
 python3 $DIR/openTdataCH--showcases/tools/ckan-utils/fetch_package_cli.py --package_id $PACKAGE_ID
+
+# Process only today and next 3 days
+for i in {0..3}; do
+    date=$(date -d "+$i days" +%Y-%m-%d)
+
+    # Check if directory exists
+    # ./data/opentransportdata.swiss/occupancy-forecast-json-dataset/occupancyforecastjson/2025-11-16
+    if [ -d "$PACKAGE_PATH/occupancyforecastjson/$date" ]; then
+        # Create operator directory
+        mkdir -p "$DIR/dist/$PACKAGE_ID/$date"
+
+        cp "$PACKAGE_PATH/occupancyforecastjson/$date/"*.json "$DIR/dist/$PACKAGE_ID/$date/."
+    fi
 done
 
-find $DIR/openTdataCH--showcases/data -type d | while read d; do
+find $DIR/dist -type d | while read d; do
     echo "--- $d ---"
     ls -al "$d"
 done
