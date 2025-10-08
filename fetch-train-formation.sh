b79e0453bcc858ce9104c79899f2868951d2e99d
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-10-08 16:45:52 +0200

    use ENV files,

--- fetch-train-formation.sh
+++ fetch-train-formation.sh
@@ -1,9 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python3 $DIR/openTdataCH--showcases/tools/ckan-utils/fetch_package_cli.py --package_id occupancy-forecast-json-dataset
-find $DIR/openTdataCH--showcases/tools/ckan-utils/data -type d | while read d; do
-    echo "--- $d ---"
-    ls -al "$d"
+PACKAGE_ID=occupancy-forecast-json-dataset
+PACKAGE_PATH=$DIR/openTdataCH--showcases/data/opentransportdata.swiss/$PACKAGE_ID
+
+python3 $DIR/openTdataCH--showcases/tools/ckan-utils/fetch_package_cli.py --package_id $PACKAGE_ID
 done
 
 find $DIR/openTdataCH--showcases/data -type d | while read d; do
